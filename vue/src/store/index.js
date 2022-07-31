c5b8728ce6e1d002c89aba9b75fd543904519508
ZacRiley1 <zacriley2@gmail.com>
2022-07-31 20:14:30 +0100

    vuex store
    Set up vuex store

---
+++ vue/src/store/index.js
@@ -0,0 +1,17 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+  state: {
+    user: {
+      data: {name: 'Zac'},
+      token: null
+    }
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {}
+
+})
+
+export default store;
